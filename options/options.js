c9d282b9d928df8015d762e2313165a706774c98
Crowdbotics <team@crowdbotics.com>
2023-10-14 18:43:45 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "motorcycle-display-43942",
+  url: "https://motorcycle-display-43942.botics.co",
+  api: "https://motorcycle-display-43942.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
